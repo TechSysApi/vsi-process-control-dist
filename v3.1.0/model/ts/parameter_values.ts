d5eeec3859abd4125c337c7370b0d71fa821cde0
s-continuum, IT265 <noreply-jenkins@post.ch>
2021-11-15 11:45:03 +0100

    v3.1.0

---
+++ v3.1.0/model/ts/parameter_values.ts
@@ -0,0 +1,20 @@
+import { ParameterValue } from './common'
+
+/**
+ * Contains exactly one main element, based on the use case (XOR).
+ */
+export interface ParameterValues {
+    /**
+     * The date+timestamp which identifies the point in time, where the information was generated, including milliseconds.
+     *
+     * @format date-time
+     */
+    timestamp: string
+
+    /**
+     * List of parameter values.
+     *
+     * @minItems 1
+     */
+    parameterValues: ParameterValue[]
+}
